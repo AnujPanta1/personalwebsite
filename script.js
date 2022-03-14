be35b583fd429d388759a37cb8dde0ffd8a958e0
Anuj Panta <anujpanta@berkeley.edu>
2022-03-13 18:11:40 -0700

    added more projects

--- script.js
+++ script.js
@@ -1,34 +1,3 @@
-
-let elem = document.getElementById("game_body");
-
-let board;
-
-function setup(){
-    canvas = createCanvas(elem.offsetWidth,elem.offsetHeight);
-    canvas.parent(elem);
-
-    board = new Board();
-}
-
-function draw(){
-    background(51);
-    board.show();
-}
-
-function windowResized(){
-    resizeCanvas(elem.offsetWidth,elem.offsetHeight);
-}
-
-class Board{
-
-    constructor(){
-        this.pos = createVector();
-        this.w = width*0.8;
-        this.h = height*0.8;
-    }
-
-    show(){
-        fill(255,0,0);
-        rect(this.pos.x,this.pos.y,this.w,this.h);
-    }
-}
+const scrollTo = function(wantedObjId){
+    document.getElementById(wantedObjId).scrollIntoView();
+};
